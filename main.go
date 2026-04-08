19f276a514874085d0a998434c1cb10414e12c01
peodiyo <perezodiyo092@gmail.com>
2026-04-08 17:59:16 +0300

    almost died

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"ascii-art-web/handlers"
@@ -8,7 +9,11 @@ import (
 
 func main() {
 	http.HandleFunc("/", handlers.HomeHandler)
-	// http.HandleFunc("ascii-art",handlers.AsciiHandler)
+	http.HandleFunc("/ascii", handlers.AsciiHandler)
 
-	http.ListenAndServe(":8080", nil)
+	log.Println("Server starting on :8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
