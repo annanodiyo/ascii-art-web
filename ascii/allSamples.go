46aba71a8a47054faff33d4c76816f1ce6728d5b
peodiyo <perezodiyo092@gmail.com>
2026-04-08 17:59:16 +0300

    almost died

--- ascii/allSamples.go
+++ ascii/allSamples.go
@@ -2,18 +2,18 @@ package ascii
 
 import "strings"
 
-func AllSamples(text string)(string, error){
-	banners := []string{"standard","shadow","thinkertoy"}
+func AllSamples(text string) (string, error) {
+	banners := []string{"standard", "shadow", "thinkertoy"}
 
 	var result strings.Builder
-	for _,banner:= range banners{
-		art,err:=Generate(text,banner)
-		if err !=nil{
-			return "",err
+	for _, banner := range banners {
+		art, err := Generate(text, banner)
+		if err != nil {
+			return "", err
 		}
-		result.WriteString("==="+banner+"===\n")
+		result.WriteString("===" + banner + "===\n")
 		result.WriteString(art)
 		result.WriteString("\n\n")
 	}
-	return result.String(),nil
+	return result.String(), nil
 }
