ff87aaf633325d131bb4f133a9a9467c39181e7e
peodiyo <perezodiyo092@gmail.com>
2026-04-14 14:15:24 +0300

    Implemented corsMiddleWare function for resource sharing

--- main.go
+++ main.go
@@ -7,12 +7,29 @@ import (
 	"ascii-art-web/handlers"
 )
 
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	http.HandleFunc("/", handlers.HomeHandler)
 	http.HandleFunc("/ascii", handlers.AsciiHandler)
 
 	log.Println("Server starting on :8080...")
-	err := http.ListenAndServe(":8080", nil)
+
+	// 👇 Wrap DefaultServeMux with middleware
+	err := http.ListenAndServe(":8080", corsMiddleware(http.DefaultServeMux))
 	if err != nil {
 		log.Fatal(err)
 	}
