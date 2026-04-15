f7010556016711d447c18b5e3a874d8c0ccd8f4d
peodiyo <perezodiyo092@gmail.com>
2026-04-15 11:00:39 +0300

    Add default text

--- handlers/handler.go
+++ handlers/handler.go
@@ -3,52 +3,54 @@ package handlers
 import (
 	"html/template"
 	"net/http"
+	"strings"
 
 	"ascii-art-web/ascii"
 )
 
 // Load template once
-var tmplate = template.Must(template.ParseFiles("templates/index.html"))
+var tmpl = template.Must(template.ParseFiles("templates/index.html"))
 
 // Struct sent to HTML
 type Response struct {
 	Result string
 	Error  string
+	Text   string
 }
 
 // GET "/"
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-
 	if r.URL.Path != "/" {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
 	}
 
-	tmplate.Execute(w, nil)
+	tmpl.Execute(w, Response{
+		Text: "TRy it out",
+	})
 }
 
 // POST "/ascii"
 func AsciiHandler(w http.ResponseWriter, r *http.Request) {
-
 	// Only POST allowed
 	if r.Method != http.MethodPost {
 		http.Error(w, "400 bad request", http.StatusBadRequest)
 		return
 	}
 
-	text := r.FormValue("text")
+	text := strings.TrimSpace(r.FormValue("text"))
 	banner := r.FormValue("banner")
 
 	// Validation
 	if text == "" {
 		w.WriteHeader(http.StatusBadRequest)
-		tmplate.Execute(w, Response{Error: "Text cannot be empty"})
+		tmpl.Execute(w, Response{Error: "Text cannot be empty"})
 		return
 	}
 
 	if banner == "" {
 		w.WriteHeader(http.StatusBadRequest)
-		tmplate.Execute(w, Response{Error: "Please select a banner"})
+		tmpl.Execute(w, Response{Error: "Please select a banner"})
 		return
 	}
 
@@ -64,9 +66,12 @@ func AsciiHandler(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		tmplate.Execute(w, Response{Error: err.Error()})
+		tmpl.Execute(w, Response{Error: err.Error()})
 		return
 	}
 
-	tmplate.Execute(w, Response{Result: result})
+	tmpl.Execute(w, Response{
+		Result: result,
+		Text:   text,
+	})
 }
