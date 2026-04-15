50fa6cf022f0d5de29d196be18969d7e843ba52f
peodiyo <perezodiyo092@gmail.com>
2026-04-15 11:31:09 +0300

    Modify Generate function to handle new lines'

--- ascii/ascii.go
+++ ascii/ascii.go
@@ -9,21 +9,36 @@ import (
 )
 
 func Generate(text, banner string) (string, error) {
+	// text = strings.ReplaceAll(text, "\\n", "\n")
 
-	// Default banner
 	if banner == "" {
 		banner = "standard"
 	}
 
-	// FIX: Correct path (REMOVED "../")
 	filepath := "banners/" + banner + ".txt"
 
 	data, err := os.ReadFile(filepath)
 	if err != nil {
 		return "", fmt.Errorf("banner not found")
 	}
+	text = strings.ReplaceAll(text, "\\n", "\n")
 
 	lines := strings.Split(string(data), "\n")
 
-	return printArt.GenerateArt(text, lines), nil
+	// text = strings.ReplaceAll(text, "\\n", "\n")
+
+	words := strings.Split(text, "\n")
+
+	var result strings.Builder
+
+	for i, word := range words {
+		art := printArt.GenerateArt(word, lines)
+		result.WriteString(art)
+
+		if i != len(words)-1 {
+			result.WriteString("\n")
+		}
+	}
+
+	return result.String(), nil
 }
